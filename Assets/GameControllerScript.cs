66fb3d1017a176afebeb3205ddff7b2b0168c824
Jubal-Norman <Jubal.norman@gmail.com>
2021-05-18 11:32:45 -0700

    Added UI and different modes as well as placing cylinders

--- Assets/GameControllerScript.cs
+++ Assets/GameControllerScript.cs
@@ -2,15 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MODE
+{
+    NONE,
+    SELECT,
+    PLACE
+}
+
 public class GameControllerScript : MonoBehaviour
 {
+    public static GameControllerScript Instance { get { return instance; } }
+    public static GameControllerScript instance;
+
     public Camera camera;
     public GameObject floor;
     public GameObject[] library;
+    public MODE currentMode;
+    public uint currentIndex;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        currentMode = MODE.NONE;
     }
 
     // Update is called once per frame
@@ -33,13 +59,23 @@ public class GameControllerScript : MonoBehaviour
             Transform objectHit = hit.transform;
 
 
-            print(objectHit.name);
             print(hit.point);
 
-            if (objectHit.gameObject.Equals(floor))
+            switch (currentMode)
             {
-                Instantiate(library[0], hit.point, Quaternion.identity);
+                case MODE.NONE:
+                    break;
+                case MODE.SELECT:
+                    print(objectHit.name + " selected");
+                    break;
+                case MODE.PLACE:
+                    if (objectHit.gameObject.Equals(floor))
+                    {
+                        Instantiate(library[currentIndex], hit.point, Quaternion.identity);
+                    }
+                    break;
             }
         }
+
     }
 }
